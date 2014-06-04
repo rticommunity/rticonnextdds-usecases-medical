c81ab85d9348d8ad43ab579cfb8367ebf931f2a8
rosemwahlin <rose@rti.com>
2014-06-03 17:28:39 -0700

    Fixed the Java script for Windows

--- MedicalDevices/ExampleCode/scripts/HMI.bat
+++ MedicalDevices/ExampleCode/scripts/HMI.bat
@@ -1,11 +1,13 @@
 @echo off
+
 setlocal
 
 set dir=%~dp0
 
 set Path=%NDDSHOME%\lib\i86Win32jdk;%PATH%
 
-cd %dir%\..\src\HMI
+
+cd %dir%\..\src\HMI\
 
 set RTI_JAR_DIR="%NDDSHOME%\class"
 set RTI_DLL_DIR="%NDDSHOME%\lib\i86Win32jdk"
@@ -15,4 +17,7 @@ set RTI_DLL_DIR=%RTI_DLL_DIR:"=%
 
 set PATH=%RTI_DLL_DIR%;%PATH%
 
-call java -classpath %dir%\..\win32\Java\AlarmHMI.jar;%RTI_JAR_DIR%\nddsjava.jar com.rti.medical.ICEAlarmDisplayApp
+call java -classpath %dir%\..\src\HMI\bin;%RTI_JAR_DIR%\nddsjava.jar com.rti.medical.ICEAlarmDisplayApp
+
+
+cd %dir%
