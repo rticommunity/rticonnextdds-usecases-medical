2f43bcf266b18d093a82e0c3e23cca7d01acc071
rosemwahlin <rose@rti.com>
2016-05-23 14:51:43 -0700

    Fixing issues in Windows scripts

--- MedicalDevices/ExampleCode/scripts/BedsideSupervisor.bat
+++ MedicalDevices/ExampleCode/scripts/BedsideSupervisor.bat
@@ -2,9 +2,50 @@
 setlocal
 
 set dir=%~dp0
-set executable_name=BedsideSupervisor
-set Path=%NDDSHOME%\lib\i86Win32VS2010;%PATH%
 
-cd %dir%\..\win32\Release\i86Win32VS2010\
+cd %dir%\..\src\BedsideSupervisor\
 
-call %executable_name% %*
+set libDir=%NDDSHOME%\lib
+
+set i86Win32Libs=i86Win32VS2015 i86Win32VS2013 i86Win32VS2012 i86Win32VS2010 i86Win32VS2008 i86Win32VS2005 i86Win32VS2003 i86Win32VC70 i86Win32VC60
+set x64Win64Libs=x64Win64VS2015 x64Win64VS2013 x64Win64VS2012 x64Win64VS2010 x64Win64VS2008 x64Win64VS2005 %i86Win32Libs%
+if "%PROCESSOR_ARCHITECTURE%"=="AMD64" (
+    set "platformsToTry=%x64Win64Libs%"
+) else if "%PROCESSOR_ARCHITEW6432%"=="AMD64" (
+    set "platformsToTry=%x64Win64Libs%"
+) else if "%PROCESSOR_ARCHITECTURE%"=="x86" (
+    set "platformsToTry=%i86Win32Libs%"
+) else (
+    echo Processor "%PROCESSOR_ARCHITECTURE%" not supported. Please contact support@rti.com.
+    exit /b 1
+)
+
+for %%a in (%platformsToTry%) do (
+    if exist "%libDir%\%%a\nddscore.dll" (
+                set platformName=%%a
+                goto break
+    )
+)
+
+@REM We need to use goto to be able to break in the for loop.
+:break
+
+@REM Return error if platformName is not set. That is, the script could not
+@REM find libraries for your environment in the install directory..
+if not defined platformName (
+    echo Cannot find libraries for your system under "%libDir%". Please install a target package.
+	exit /b 1
+)
+
+set RTI_JAR_DIR="%NDDSHOME%\lib\java"
+set RTI_DLL_DIR="%NDDSHOME%\lib\%platformName%"
+
+REM Remove any existing quotes
+set RTI_DLL_DIR=%RTI_DLL_DIR:"=%
+
+set PATH=%RTI_DLL_DIR%;%PATH%
+
+call java -classpath %dir%\..\src\BedsideSupervisor\bin;%RTI_JAR_DIR%\nddsjava.jar com.rti.medical.BedsideSupervisor
+
+
+cd %dir%
--- MedicalDevices/ExampleCode/scripts/DeviceData.bat
+++ MedicalDevices/ExampleCode/scripts/DeviceData.bat
@@ -1,8 +1,8 @@
 @echo off
 setlocal
 
-IF []==[%REPLAY_HOME%] (
-  @echo The REPLAY_HOME environment variable must be set to Connext_install_dir/RTI Recording Service 5.1.0 to run this example
+IF []==[%NDDSHOME%] (
+  @echo The NDDSHOME environment variable must be set to install_dir\rti_connext_dds-x.x.x to run this example
 )
 
 set dir=%~dp0
@@ -12,8 +12,8 @@ set executable_name=rtireplay
 cd %dir%\..\replay
 
 REM Remove quotation marks
-set REPLAY_HOME=%REPLAY_HOME:"=%
+set NDDSHOME=%NDDSHOME:"=%
 
 
-start "ECG Replay" "%REPLAY_HOME%\bin\%executable_name%" -cfgFile device_replay.xml -cfgName replayECGDevice
-start "Pulse Oximeter Replay" "%REPLAY_HOME%\bin\%executable_name%" -cfgFile device_replay.xml -cfgName replayPODevice
+start "ECG Replay" "%NDDSHOME%\bin\%executable_name%" -cfgFile device_replay.xml -cfgName replayECGDevice
+start "Pulse Oximeter Replay" "%NDDSHOME%\bin\%executable_name%" -cfgFile device_replay.xml -cfgName replayPODevice
